71ab83b88a00e7fe0415161891aa5450985da34f
Omotosho Kamalideen <dapious99@gmail.com>
2024-07-14 19:48:48 +0100

    clean up

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,33 +4,30 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-cyan-950 text-white px-8 py-3 text-center flex justify-between items-center">
-      <p className="">
+      <p className="hover:text-amber-600">
         &copy; {currentYear} shotMe by Kodewarlock. All rights reserved.
       </p>
       <div className="flex gap-4">
         <a
           href="#"
           aria-label="Twitter"
-          aria-hidden="true"
           className="hover:text-amber-600 transition-colors cursor-pointer"
         >
-          <i className="fab fa-twitter"></i>
+          <i className="fab fa-twitter" aria-hidden="true"></i>
         </a>
         <a
           href="#"
           aria-label="Facebook"
-          aria-hidden="true"
           className="hover:text-amber-600 transition-colors cursor-pointer"
         >
-          <i className="fab fa-facebook-f"></i>
+          <i className="fab fa-facebook-f" aria-hidden="true"></i>
         </a>
         <a
           href="#"
           aria-label="Instagram"
-          aria-hidden="true"
           className="hover:text-amber-600 transition-colors cursor-pointer"
         >
-          <i className="fab fa-instagram"></i>
+          <i className="fab fa-instagram" aria-hidden="true"></i>
         </a>
       </div>
     </footer>
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
   return (
     <div className="flex justify-between bg-cyan-900 dark:bg-cyan-950 text-white cursor-pointer px-8 py-3">
-      <Link href="/" className="italic">
+      <Link href="/" className="italic text-lg font-semibold text-amber-600">
         shotMe
       </Link>
       <div className="flex items-center gap-4">
@@ -22,11 +22,21 @@ const Navbar = () => {
           )}
         </button>
         <div className="flex gap-6">
-          <Link href="/picture">Pictures</Link>
-          <Link href="/video">Videos</Link>
-          <Link href="/about">About</Link>
-          <Link href="/signin">Sign In</Link>
-          <Link href="/signup">Sign Up</Link>
+          <Link href="/picture" className="hover:text-amber-600">
+            Pictures
+          </Link>
+          <Link href="/video" className="hover:text-amber-600">
+            Videos
+          </Link>
+          <Link href="/about" className="hover:text-amber-600">
+            About
+          </Link>
+          <Link href="/signin" className="hover:text-amber-600">
+            Sign In
+          </Link>
+          <Link href="/signup" className="hover:text-amber-600">
+            Sign Up
+          </Link>
         </div>
       </div>
     </div>
