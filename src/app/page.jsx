f4f7d49399662273daeb1e9333174e4013c0a192
Omotosho Kamalideen <dapious99@gmail.com>
2024-07-16 06:56:18 +0100

    clean up

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,32 +1,3 @@
-// import Link from "next/link";
-// import { FaCamera } from "react-icons/fa";
-// import { IoIosArrowForward } from "react-icons/io";
-// import { BsFillCameraReelsFill } from "react-icons/bs";
-
-// export default function Home() {
-//   return (
-//     <main className="min-h-screen px-20 py-11">
-//       <p className="text-center text-2xl">
-//         Welcome to <span className="italic">shotMe</span>
-//       </p>
-//       <div className="flex gap-4">
-//         <Link href="/picture" className="">
-//           <FaCamera className="size-60 text-amber-900 hover:animate-ping" />
-//           <p className="inline-flex items-center gap-1 font-medium mt-2">
-//             Click here to take your shot! <IoIosArrowForward />
-//           </p>
-//         </Link>
-//         <Link href="/video" className="">
-//           <BsFillCameraReelsFill className="size-56 text-amber-900 hover:animate-ping" />
-//           <p className="inline-flex items-center gap-1 font-medium mt-6">
-//             Click here to record a video! <IoIosArrowForward />
-//           </p>
-//         </Link>
-//         <div></div>
-//       </div>
-//     </main>
-//   );
-// }
 import Link from "next/link";
 import { FaCamera } from "react-icons/fa";
 import { IoIosArrowForward } from "react-icons/io";
